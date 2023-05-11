f075f3e5de20ca2b6a27345fe91b81f7ae14cd57
Asitha Fernando <asithachristo@gmail.com>
2023-05-12 02:22:37 +0530

    Add main.tf file

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.66.1"
+    }
+  }
+}
+
+provider "aws" {
+    profile = "default"
+    region  = "us-east-1"
+}
+
+resource "aws_instance" "af_server" {
+  ami           = "ami-0889a44b331db0194"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AFServer"
+  }
+}
